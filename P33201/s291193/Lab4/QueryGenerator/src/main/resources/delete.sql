e8415f1c984a346a07feeec1e4d49114987a5eb8
Maybenex1ime <74535702+Maybenex1ime@users.noreply.github.com>
2022-11-20 23:55:07 +0300

    Add files via upload

---
+++ P33201/s291193/Lab4/QueryGenerator/src/main/resources/delete.sql
@@ -0,0 +1,6 @@
+DROp TABLE spo;
+DROP TABLE order_;
+DROP TABLE people;
+DROP TABLE part;
+DROP TABLE transport;
+DROP TABLE salvage
---
+++ P33201/s291193/Lab4/QueryGenerator/target/classes/delete.sql
@@ -0,0 +1,6 @@
+DROp TABLE content_salvage;
+DROP TABLE order_;
+DROP TABLE people;
+DROP TABLE part;
+DROP TABLE transport;
+DROP TABLE salvage
